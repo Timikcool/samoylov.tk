63997fce0d90edb036d5c7a10929eec28f9bb697
Timikcool <timikcool@yandex.ru>
2019-04-08 19:18:47 +0300

    better descriptions

--- src/components/trophies/trophies.js
+++ src/components/trophies/trophies.js
@@ -16,7 +16,7 @@ const Trophies = () => {
       title: "Imaguru FinTech Hackathon: Elevator Lab Edition",
       status: "Winner",
       description:
-        "Our project is a SaaS solution for every business with call centre. Especially large ones. It analyzes all call records and provides deep analytics about each one based on speech-to-text technology and our algorithms to our customers.",
+        "Our project is a SaaS solution for every business with call centre. Especially large ones. It analyzes all call records and provides deep analytics about each one based on speech-to-text technology and our cool algorithms.",
       date: "24.03.2019",
       link: "https://imaguru.by/event/fintech-hackathon/",
       images: [fintechTeam19, fintechMe19]
@@ -25,16 +25,16 @@ const Trophies = () => {
       title: "Imaguru International Blockchainthon",
       status: "2nd place",
       description:
-        "We were building decentrilized gambling game for DAO.Casino protocol and at the end we've got proof-of-work game based on smart contract deployed to Ethereum testnet",
+        "We've build decentrilized gambling game for DAO.Casino protocol and at the end we've got 2eth for lottery game based on smart contract deployed to Ethereum testnet",
       date: "07.10.2018",
       link: "https://imaguru.by/event/imaguru-blockchain-hackathon-4/",
       images: [blockchainMe18, blockchainTeam18_2, blockchainTeam18]
     },
     {
       title: "Belarus ICT Start-up Award",
-      status: "Finalist",
+      status: "Finalist (top 15 out of 300)",
       description:
-        "The first startup I tried to launch was about sport communities. I tried to build a social platform for team games players to find members, teams, playgrounds and sport organizations.",
+        "The first startup I tried to launch was about sport communities. I tried to build a social platform for team games players and sport organizations to find members, teams, playgrounds and have fun.",
       date: "20.04.2017",
       link: "https://tech.onliner.by/2017/04/20/poxudet",
       images: [ictAwardMe17]
